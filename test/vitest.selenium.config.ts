0321928e6ea213809f1282cc069d1230cf852d95
Marta <alu0101559513@ull.edu.es>
2025-12-02 12:36:34 +0000

    pruebas básicas

---
+++ test/vitest.selenium.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+
+    sequence: {
+      concurrent: false,  
+    },
+
+    testTimeout: 30000,
+    hookTimeout: 30000,
+  },
+});
